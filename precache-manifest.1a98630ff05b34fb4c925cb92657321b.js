de71f4f56e1dd4a54d1f16a303b5084575bf5dfd
Jeremy Downes <jeremydownes@hotmail.com>
2020-10-09 13:25:30 -0400

    Updates

---
+++ precache-manifest.1a98630ff05b34fb4c925cb92657321b.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914edec370ded2eb347b93c1efc18788",
+    "url": "/santa-assault/index.html"
+  },
+  {
+    "revision": "6cf4751e9e8ca150310a",
+    "url": "/santa-assault/static/css/main.4ce92437.chunk.css"
+  },
+  {
+    "revision": "642fa23e83e948d35453",
+    "url": "/santa-assault/static/js/2.98c3c917.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/santa-assault/static/js/2.98c3c917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf4751e9e8ca150310a",
+    "url": "/santa-assault/static/js/main.5c722ed7.chunk.js"
+  },
+  {
+    "revision": "2704d0cfb432e6bccbbb",
+    "url": "/santa-assault/static/js/runtime-main.a7849926.js"
+  },
+  {
+    "revision": "29bbbfd86dc3062342c5679be60810ab",
+    "url": "/santa-assault/static/media/arrow-left.29bbbfd8.png"
+  },
+  {
+    "revision": "cf8d72c56dfccde41dbb4b9f1c77ea75",
+    "url": "/santa-assault/static/media/arrow-right.cf8d72c5.png"
+  },
+  {
+    "revision": "00ffb0b1f27ef739cad7a5e7b2f4a2c4",
+    "url": "/santa-assault/static/media/background.00ffb0b1.jpg"
+  },
+  {
+    "revision": "ea3ead6dad83fba8fb4811e7157e386f",
+    "url": "/santa-assault/static/media/background.ea3ead6d.png"
+  },
+  {
+    "revision": "96665d790e86005eea6e583b83d53f96",
+    "url": "/santa-assault/static/media/badKid-left.96665d79.gif"
+  },
+  {
+    "revision": "9dcae8479473cb84ec360ea5e1901a96",
+    "url": "/santa-assault/static/media/badKid-right.9dcae847.gif"
+  },
+  {
+    "revision": "9af0db17432d06dfef8d9f1ec8e202ad",
+    "url": "/santa-assault/static/media/bg.9af0db17.png"
+  },
+  {
+    "revision": "1d95fec4c306845293b0b53cbfbe525b",
+    "url": "/santa-assault/static/media/candy-cane.1d95fec4.png"
+  },
+  {
+    "revision": "9ecf4b38718650ce95b1a468d7a8cf20",
+    "url": "/santa-assault/static/media/candy-cane.9ecf4b38.jpg"
+  },
+  {
+    "revision": "fbf6fb6c18cca606ed8db8cf2d7780f0",
+    "url": "/santa-assault/static/media/coal-bucket.fbf6fb6c.png"
+  },
+  {
+    "revision": "f7968ebb72874f2af7ae015a7c31371c",
+    "url": "/santa-assault/static/media/coal.f7968ebb.png"
+  },
+  {
+    "revision": "227591d0aec3da98e89e1b164875ead7",
+    "url": "/santa-assault/static/media/coke.227591d0.png"
+  },
+  {
+    "revision": "7302cf333527d0be0c34779a56182c86",
+    "url": "/santa-assault/static/media/cookies.7302cf33.png"
+  },
+  {
+    "revision": "d6f7b64c76a13396d7d447fd72ba42ab",
+    "url": "/santa-assault/static/media/elf.d6f7b64c.gif"
+  },
+  {
+    "revision": "5d121faff474a3cf59a6fa9afa29cd13",
+    "url": "/santa-assault/static/media/fairy.5d121faf.gif"
+  },
+  {
+    "revision": "9f1b98a39f7dc886036bad96fcfb9554",
+    "url": "/santa-assault/static/media/gift.9f1b98a3.png"
+  },
+  {
+    "revision": "ab16915da837bce88adb4cb1c4b46380",
+    "url": "/santa-assault/static/media/gift.ab16915d.gif"
+  },
+  {
+    "revision": "0ba191ea4c736e6f42a63771ce8ff632",
+    "url": "/santa-assault/static/media/ginger.0ba191ea.gif"
+  },
+  {
+    "revision": "4cbd998ec69210819ae75d0669165942",
+    "url": "/santa-assault/static/media/gumdrop.4cbd998e.png"
+  },
+  {
+    "revision": "aaf0aabda777804add09226acfc70b4d",
+    "url": "/santa-assault/static/media/instructions-mobile.aaf0aabd.png"
+  },
+  {
+    "revision": "8ba29d1bf30034e1a73fd1fc5afcf413",
+    "url": "/santa-assault/static/media/instructions.8ba29d1b.png"
+  },
+  {
+    "revision": "02d76d4a7db08140958340fa1efe516c",
+    "url": "/santa-assault/static/media/instructions1-mobile.02d76d4a.png"
+  },
+  {
+    "revision": "67a0ff062aba5eb3fb56fe0fe9b348eb",
+    "url": "/santa-assault/static/media/instructions1.67a0ff06.png"
+  },
+  {
+    "revision": "d798aff93306c1fa732303515adfdbb6",
+    "url": "/santa-assault/static/media/instructions2-mobile.d798aff9.png"
+  },
+  {
+    "revision": "c878f82296b6e915cf292906da3a277c",
+    "url": "/santa-assault/static/media/instructions2.c878f822.png"
+  },
+  {
+    "revision": "4eb9ec393c64ee5870abb8fd574e681a",
+    "url": "/santa-assault/static/media/instructions3-mobile.4eb9ec39.png"
+  },
+  {
+    "revision": "deeaa1e2db65627c4143331bcf01cf87",
+    "url": "/santa-assault/static/media/instructions3.deeaa1e2.png"
+  },
+  {
+    "revision": "f8bf7aa6ba07df4801df8249da1d4658",
+    "url": "/santa-assault/static/media/jump.f8bf7aa6.gif"
+  },
+  {
+    "revision": "4a55540517848aaebed48dc77f364172",
+    "url": "/santa-assault/static/media/letter.4a555405.png"
+  },
+  {
+    "revision": "6b7ad636f5ecbbafdd6b70d7e8d494b4",
+    "url": "/santa-assault/static/media/nut.6b7ad636.png"
+  },
+  {
+    "revision": "a8900d709ea00f846d1ee4ab81331fa4",
+    "url": "/santa-assault/static/media/nuts.a8900d70.png"
+  },
+  {
+    "revision": "43dcf2265283ed0888900fcb42bd41f6",
+    "url": "/santa-assault/static/media/run.43dcf226.gif"
+  },
+  {
+    "revision": "40ae6ceb9e46d112c747e8f08bff659b",
+    "url": "/santa-assault/static/media/salt.40ae6ceb.png"
+  },
+  {
+    "revision": "3b0ae5dfbfa77a18778205c4b252fc28",
+    "url": "/santa-assault/static/media/sleigh.3b0ae5df.png"
+  },
+  {
+    "revision": "9b20520b7076ef24075d5c817c4cbb5d",
+    "url": "/santa-assault/static/media/snowman.9b20520b.gif"
+  },
+  {
+    "revision": "c6b4eaaed59cf6afc17d7ccb5d6f6942",
+    "url": "/santa-assault/static/media/splash-screen-mobile.c6b4eaae.jpg"
+  },
+  {
+    "revision": "e7063692fec91d590a1b8e39d7991b1b",
+    "url": "/santa-assault/static/media/standing.e7063692.gif"
+  },
+  {
+    "revision": "18c4409a2f3b42ccf255238cd09d7ed5",
+    "url": "/santa-assault/static/media/throw.18c4409a.gif"
+  },
+  {
+    "revision": "a982c44174136c1e30c84a08ea9fed9f",
+    "url": "/santa-assault/static/media/toy.a982c441.gif"
+  },
+  {
+    "revision": "08428c38eb3bdccb6d0807bf412a8e84",
+    "url": "/santa-assault/static/media/wreath.08428c38.png"
+  }
+]);
